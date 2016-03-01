f46585521b5323d9ffe2c4bc4a2a871d6db3bcb8
Emily McCracken <emilymcc803@gmail.com>
2016-03-01 11:08:43 -0500

    added icons and content to 'at a glance' section, changed nav bar color for JS

--- stylesheets/main.js
+++ stylesheets/main.js
@@ -1,23 +1,8 @@
-// $(document).ready(function(){     
+$(document).ready(function(){
 
-//    var scroll_start = 0;
-//    var startchange = $('#startchange');
-//    var offset = startchange.offset();
-
-//    $(document).scroll(function() { 
-//       scroll_start = $(this).scrollTop();
-//       if(scroll_start > offset.top) {
-//           $('#navbar').css('background-color', '#f0f0f0');
-//        } else {
-//           $('#navbar').css('background-color', '#1f8dd6');
-//        }
-//    });
-// });
-
-    $(document).ready(function(){
       $(window).scroll(function() { 
-        if ($(document).scrollTop() > 520) {
-          $(".navbar").css("background-color", "#1f8dd6"); 
+        if ($(document).scrollTop() > 510) {
+          $(".navbar").css("background-color", "#043D5D"); 
         } else {
           $(".navbar-fixed-top").css("background-color", "transparent");
         }
