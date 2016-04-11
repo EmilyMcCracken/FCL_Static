ef53fc346fb967ef69126e1b07a94a709616f27e
Emily McCracken <emilymcc803@gmail.com>
2016-04-10 20:43:35 -0400

    version 4 sent to clients, removed image header, enlarged logo, added social media icons to nav bar, made navy nav permanent, changed contact

--- stylesheets/main.js
+++ stylesheets/main.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-      $(window).scroll(function() { 
-        if ($(document).scrollTop() > 510) {
-          $(".navbar").css("background-color", "#043D5D"); 
-        } else {
-          $(".navbar-fixed-top").css("background-color", "transparent");
-        }
-      });
-    });
+//       $(window).scroll(function() { 
+//         if ($(document).scrollTop() > 510) {
+//           $(".navbar").css("background-color", "#233146"); 
+//           $(".sand").css("color", "#dbcfbf"); 
+//           $(".top_add").css("color", "#dbcfbf"); 
+//         } else {
+//           $(".navbar-fixed-top").css("background-color", "transparent");
+//         }
+//       });
+//     });
